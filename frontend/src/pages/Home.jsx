9fccaa35867b4e375674626b70f3c2c19a2c8eed
valgeorg97 <valentingg1997@gmail.com>
2024-04-15 15:17:19 +0300

    Add Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { PORT } from '../constants';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
+import logo from '../assets/logo.png'
 
 const Home = () => {
     const [question, setQuestion] = useState({});
@@ -9,26 +10,80 @@ const Home = () => {
     const [score, setScore] = useState(0);
     const [loading, setLoading] = useState(false);
 
-    useEffect(() =>{
+    useEffect(() => {
         setLoading(true);
         axios.get(`http://localhost:${PORT}`)
-        .then((res) =>{
-            setQuestion(res.data);
+            .then((res) => {
+                setQuestion(res.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoading(false);
+            })
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const response = await axios.post(`http://localhost:${PORT}`, {
+                country: question.country,
+                userAnswer: userAnswer
+            });
+            const data = response.data;
+            if (data.isCorrect) {
+                setScore(score + 1);
+            } else {
+                setScore(0);
+                setUserAnswer('');
+            }
             setLoading(false);
-        })
-        .catch((err) =>{
-            console.log(err);
+            setUserAnswer('');
+            setQuestion(data.question);
+        } catch (error) {
             setLoading(false);
-        })
-    }, [])
+            console.error('Error submitting answer:', error);
+        }
+    };
 
+    return (
+         <div className="flex flex-col justify-start items-center bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+                <img src={logo} alt="Logo" className="h-52 w-auto mb-4 self-center" />
+                <div className="container shadow-xl max-w-lg mx-auto border border-gray-300 rounded-md px-4 py-8 bg-slate-50">
+                    <div className="flex justify-center items-center mb-4">
+                        <div className="font-bold text-xl">Total Points: {score}</div>
+                    </div>
+                {loading ? (
+                    <div className="flex justify-center items-center mb-4">
+                        <Spinner />
+                    </div>
+                ) : (
+                    <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+                        <div className="mb-4">
+                            <label htmlFor="answer" className="block text-gray-700 font-bold mb-2">
+                                Which is the capital of {question.country}?
+                            </label>
+                            <input
+                                type="text"
+                                id="answer"
+                                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                                value={userAnswer}
+                                onChange={(e) => setUserAnswer(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className='flex justify-center'>
+                        <button type="submit" className="bg-gradient-to-r from-blue-500 to-green-400 
+                        hover:from-green-400 hover:to-blue-500 text-white px-4 py-2 rounded-md items-center flex">
+                            Submit
+                        </button>
+                        </div>
+                    </form>
+                )}
+            </div>
+        </div>
+    );
+};
 
-
-  return (
-    <div>
-     {question.country} - {question.capital}
-    </div>
-  )
-}
-
-export default Home
+export default Home;
