ab59880033c88632e682c566275e3c0fb3d969a4
valgeorg97 <valentingg1997@gmail.com>
2024-04-15 10:30:22 +0300

    Fetch question with axios

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PORT } from '../constants';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
 
 const Home = () => {
-
     const [question, setQuestion] = useState({});
     const [userAnswer, setUserAnswer] = useState('');
     const [score, setScore] = useState(0);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() =>{
+        setLoading(true);
+        axios.get(`http://localhost:${PORT}`)
+        .then((res) =>{
+            setQuestion(res.data);
+            setLoading(false);
+        })
+        .catch((err) =>{
+            console.log(err);
+            setLoading(false);
+        })
+    }, [])
+
 
 
   return (
     <div>
-      home
+     {question.country} - {question.capital}
     </div>
   )
 }
