96bbe67e66712f026066cc65f1fb023904837a10
valgeorg97 <valentingg1997@gmail.com>
2024-04-16 10:07:05 +0300

    Add Dist Folder

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,12 @@ app.post("/", async (req, res) => {
     }
 });
 
+app.use(express.static(path.join(__dirname, "./frontend/dist")));
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
