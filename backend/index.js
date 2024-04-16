7f46b469e06e68f844ec2e71c2ce3754cacc89ec
valgeorg97 <valentingg1997@gmail.com>
2024-04-16 10:31:36 +0300

    Update dist path

--- backend/index.js
+++ backend/index.js
@@ -57,10 +57,10 @@ app.post("/", async (req, res) => {
     }
 });
 
-app.use(express.static(path.join(__dirname, "./frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) =>{
-    res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 app.listen(PORT, () => {
