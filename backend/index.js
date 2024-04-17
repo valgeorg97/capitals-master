be9284b02415495f7bd90a3ca777ddcbc2322080
valgeorg97 <valentingg1997@gmail.com>
2024-04-17 09:40:49 +0300

    Cut unused code

--- backend/index.js
+++ backend/index.js
@@ -57,12 +57,6 @@ app.post("/", async (req, res) => {
     }
 });
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) =>{
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
